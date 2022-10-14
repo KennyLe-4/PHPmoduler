30785b99d7a3891b5995f47f347188a212c5ecf7
Kenny Le <79541722+KennyLe-4@users.noreply.github.com>
2022-10-14 20:20:32 +0200

    modul 5

---
+++ Modul 5/index.php
@@ -0,0 +1,33 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet">
+
+
+
+</head>
+<div class = "center"> 
+    <h1>Velkommen til siden min</h1>
+    <h2>Nedenfor kan du se oppgavene mine</h2>
+    <a href="Opg1.php"> Oppgave 1 - standardavvik </a>
+    <br>
+    <a href="Opg2.php">Oppgave 2 - plan for filstruktur og klassebibliotek </a>
+    <br>
+    <a href="Opg3.php">Oppgave 3 -  kommentering </a>
+    <br>
+    <a href="Opg4.php">Oppgave 4 - god programmeringsskikk  </a>
+    <br>
+    <a href="Opg5.php">Oppgave 5 - kryptering </a>
+
+</div>
+<body>
+    
+</body>
+</html>
